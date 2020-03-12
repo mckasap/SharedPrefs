2e555b7ad097ef721477149a3f893eaf81d64ce8
mckasap <mckasap@gmail.com>
2020-03-12 14:50:09 +0300

    Shared Preference eklendi

--- app/src/main/java/org/kasapbasi/sharedprefs/MainActivity.java
+++ app/src/main/java/org/kasapbasi/sharedprefs/MainActivity.java
@@ -2,13 +2,43 @@ package org.kasapbasi.sharedprefs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText et;
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        String str= et.getText().toString();
+        SharedPreferences.Editor edit= pref.edit();
+        edit.putString("theKey",str);
+        edit.commit();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+    SharedPreferences pref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            et=(EditText) findViewById(R.id.theText);
+
+         pref= getSharedPreferences("TEST",MODE_PRIVATE);
+        String str=pref.getString("theKey","");
+        et.setText(str);
+
+
     }
+
+
+
 }
